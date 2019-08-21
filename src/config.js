193b08efe3f67e8be778d8322f967fd87273b520
Brion Mario <info@brionmario.com>
2019-08-21 14:09:19 +0530

    Change URLs from HTTP to HTTPS

--- src/config.js
+++ src/config.js
@@ -3,10 +3,11 @@ export const CONFIG = {
     "AUTHORIZE_ENDPOINT": "https://localhost:9443/oauth2/authorize",
     "RESPONSE_TYPE": "code",
     "SCOPE": "openid",
-    "REDIRECT_URI": "http://localhost:3000",
+    "REDIRECT_URI": "https://localhost:9000",
     "CLIENT_ID": "fP11CnMhhaUCiLxTr9FPyVQSDJAa",
     "CLIENT_SECRET": "1Dhb6PtLo5Us9iBPLsfW83GY28Ua",
     "GRANT_TYPE": "authorization_code",
-    "CLIENT_URL": "http://localhost:3000",
-    "LOGOUT_URL": "https://localhost:9443/oidc/logout"
+    "CLIENT_URL": "https://localhost:9000",
+    "LOGOUT_URL": "https://localhost:9443/oidc/logout",
+    "COOKIE_PATH": "/"
 };
